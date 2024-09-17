c7e3f0ea4fe338e11411b29210d42ec5d661ff70
Johan Rong <59508244+johron@users.noreply.github.com>
2024-09-17 20:18:35 +0200

    Only display hello in languages is can comminicate in

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let greetings = ["Hello", "Hola", "Bonjour", "Hallo", "Ciao", "Olá", "Привет", "مرحبا", "नमस्ते", "Hej", "Γειά σου", "Ahoj", "Szia", "Selam", "שלום"];
+let greetings = ["Hello", "Hola", "Hallo", "Hei"];
 let currentTick = 0;
 let is_first_tick = true;
 let fadeDuration = 500; // duration of the fade animation in milliseconds
